2ecb03e4a9f6b0b11db6cf6f1e11069782ab4739
bombichino <60117085+bombichino@users.noreply.github.com>
2020-07-02 22:23:40 -0400

    first component and app creation

---
+++ convenienc/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import LoginBtn from "./components/LoginBtn";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>convenienC Testing</h1>
+      <LoginBtn /> 
+    </div>
+  
+  );
+}
+
+export default App;
