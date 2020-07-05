70f98727f4146d6d264ba69d14c8fab6abc5d9d3
Caroline Ninganga <carolninganga@gmail.com>
2020-07-05 10:36:28 -0400

    added signup page

--- convenienc/src/App.js
+++ convenienc/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 // import logo from './logo.svg';
 import LoginBtn from "./components/LoginBtn";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import OurTeam from './pages/OurTeam/OurTeam';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>convenienC Testing</h1>
+    <BrowserRouter>
+      <Navbar />
       <LoginBtn /> 
-    </div>
-  
+      <Route exact path='/' component={Home} />
+      <Route path='/ourTeam' component={OurTeam} />
+    </BrowserRouter> 
   );
 }
+
 
 export default App;
---
+++ convenienc/src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OurTeam() {
+    return (
+        <div>
+           <h1>Contact us</h1> 
+        </div>
+    )
+}
+ 
+export default OurTeam;
