f6f664c26a4bd5f9b91e4f91e221785227a83d63
abiryusuf <yusuf_ctg@yahoo.com>
2020-07-08 22:18:36 -0500

    homepage

--- convenienc/src/pages/Home/Home.js
+++ convenienc/src/pages/Home/Home.js
@@ -1,16 +1,8 @@
-
-import React, { Component } from 'react';
-import styles from'./styles.css';
-import { FormBtn } from '../../components/Form';
-import { Redirect } from 'react-router-dom';
-;
-
 import React, { Component } from "react";
 //import { FormBtn } from '../../components/Form';
 import { Redirect } from "react-router-dom";
 import ClickBtn from "../../components/ClickBtn/ClickBtn";
 import "./home-style.css";
-
 // import businessSignUp from '../BusinessSignUp/BusinessSignUp';
 
 class Home extends Component {
@@ -33,37 +25,6 @@ class Home extends Component {
 
   render() {
     return (
-
-        <div className = "singup">
-            <FormBtn
-
-        <div>
-            <ClickBtn
-
-			    disabled={!(this.state.email && this.state.password)}
-        className={styles.singup}onClick={this.handleFormSubmit}
-        
-				>
-<<<<<<< HEAD
-				
-                <button type="submit" onClick={this.handleFormSubmit} className="btn btn-success">
-                Business SignUp
-        </button>
-		    </FormBtn>
-        
-=======
-				Business SignUp
-		    </ClickBtn>
->>>>>>> 6df25ff9354830cd3ce10c337b2873de7e6be282
-            {this.props.user && this.props.user._id ? <Redirect to='/home' /> : <></>}
-        </div>   
-       
-       
-    )}   
-};
- 
-export default Home;
-=======
       <div>
         <div className="home-page">
           <ClickBtn
@@ -84,4 +45,3 @@ export default Home;
 }
 
 export default Home;
->>>>>>> a065f2a94bb9f05215c0dd5357dbe4b9bccc82f6
