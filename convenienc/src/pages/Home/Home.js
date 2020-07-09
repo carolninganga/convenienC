06218348569804e03bd49bea0588c89f4ea3ac4b
bombichino <60117085+bombichino@users.noreply.github.com>
2020-07-08 21:29:54 -0400

    updated the css and amended file structures, renamed css and js files

--- convenienc/src/pages/Home/Home.js
+++ convenienc/src/pages/Home/Home.js
@@ -1,40 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import { FormBtn } from '../../components/Form';
-import { Redirect } from 'react-router-dom';
-import ClickBtn from '../../components/ClickBtn';
+import { Redirect } from "react-router-dom";
+import ClickBtn from "../../components/ClickBtn/ClickBtn";
+import "./home-style.css";
 // import businessSignUp from '../BusinessSignUp/BusinessSignUp';
 
-
 class Home extends Component {
-    constructor(props) {
-		super(props);
-		this.state = {
-			email: '',
-			username: '',
-			password: '',
-			passwordConf: ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      username: "",
+      password: "",
+      passwordConf: "",
+    };
+  }
 
-    handleFormSubmit = event => {
-        event.preventDefault();
-		if (this.state.email && this.state.password) {
-			console.log(this.state.email);
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.email && this.state.password) {
+      console.log(this.state.email);
     }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="home-page">
+          <ClickBtn
+            disabled={!(this.state.email && this.state.password)}
+            onClick={this.handleFormSubmit}
+          >
+            Business SignUp
+          </ClickBtn>
+          {this.props.user && this.props.user._id ? (
+            <Redirect to="/home" />
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-    render() {
-    return (
-        <div>
-            <ClickBtn
-			    disabled={!(this.state.email && this.state.password)}
-				onClick={this.handleFormSubmit}
-				>
-				Business SignUp
-		    </ClickBtn>
-            {this.props.user && this.props.user._id ? <Redirect to='/home' /> : <></>}
-        </div>   
-    )}   
-};
- 
 export default Home;
