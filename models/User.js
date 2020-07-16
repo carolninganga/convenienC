f47fdd4f3ccb3be0403316434db01ca30b50748e
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-15 22:17:12 -0400

    added new files

--- models/User.js
+++ models/User.js
@@ -1,53 +1,24 @@
-var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 
-var UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true,
-  }
-});
-
-//hashing a password before saving it to the database
-UserSchema.pre('save', function (next) {
-  var user = this;
-  bcrypt.hash(user.password, 14, function (err, hash){
-    if (err) {
-      return next(err);
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
-    user.password = hash;
-    next();
-  })
 });
 
-//authenticate input against database
-UserSchema.statics.authenticate = function (email, password, callback) {
-  User.findOne({ email: email })
-    .exec(function (err, user) {
-      if (err) {
-        return callback(err)
-      } else if (!user) {
-        var err = new Error('User not found.');
-        err.status = 401;
-        return callback(err);
-      }
-      bcrypt.compare(password, user.password, function (err, result) {
-        
-        if(err) throw err;
 
-        if (result === true) {
-          return callback(null, user);
-        } else {
-          return callback();
-        }
-      })
-    });
-}
-var User = mongoose.model('User', UserSchema);
-module.exports = User;
+module.exports = mongoose.model('user', UserSchema);
